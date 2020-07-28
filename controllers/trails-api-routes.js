011ab7b27de3cc350afd43ca977263636496ba37
Sierra Chapman <siechap@gmail.com>
2020-07-27 21:07:13 -0700

    add skeleton API routes for users

--- controllers/trails-api-routes.js
+++
@@ -1,7 +0,0 @@
-// this file creates a router than handles the api routes for trail data
-
-// import dependencies
-const express = require("express");
-
-// export router
-module.exports = express.Router();

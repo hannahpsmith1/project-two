9aa3ec2e97a718efb5bbda214e5d87ef7592775b
Sierra Chapman <siechap@gmail.com>
2020-07-27 21:14:18 -0700

    add skeleton API routes for entries

--- controllers/entries-api-routes.js
+++ controllers/entries-api-routes.js
@@ -3,5 +3,23 @@
 // import dependencies
 const express = require("express");
 
+// create router
+const router = express.Router();
+
+// add an entry
+router.post("/api/users/:user_id/entries", (req, res) => {
+  res.send("API route for adding an entry belonging to user id " + req.params.user_id);
+});
+
+// update an entry
+router.put("/api/users/:user_id/entries/:entry_id", (req, res) => {
+  res.send("API route for updating entry with id " + req.params.entry_id + " belonging to user id " + req.params.user_id);
+});
+
+// delete an entry
+router.delete("/api/users/:user_id/entries/:entry_id", (req, res) => {
+  res.send("API route for deleting entry with id " + req.params.entry_id + " belonging to user id " + req.params.user_id);
+});
+
 // export router
-module.exports = express.Router();
+module.exports = router;

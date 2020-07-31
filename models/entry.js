602a884a1da04d91ebc652e46235c4c970aa8891
Hannah Smith <hannahpsmith1@gmail.com>
2020-07-30 20:34:28 -0700

    adding tables to models

---
+++ models/entry.js
@@ -0,0 +1,45 @@
+module.exports = function(sequelize, DataTypes) {
+    var Entry = sequelize.define("entry", {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+          },
+        title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      type: {
+        type: DataTypes.STRING,
+        // do we want to allow them not to have a name? if not I have the validation there
+        allowNull: true,
+        // validate: {
+        //   len: [1]
+        // }
+      },
+      description: {
+        type: DataTypes.STRING(30000),
+        defaultValue: "Pacific Crest Trail"
+      },
+      zipCode: {
+        type: DataTypes.INTEGER,
+        defaultValue: "55555"
+      },
+      dateHiked: {
+        type: DataTypes.DATEONLY,
+        defaultValue: "MM/DD/YYYY"
+      }
+    //   individualRating: {
+    //     type: DataTypes.INTEGER
+    //   }
+    // Maybe we should put the rating here? 
+    });
+    return Entry;
+
+
+  };

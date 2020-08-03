5ddf369174796e16fbe0a5298799188b4e740547
Hannah Smith <hannahpsmith1@gmail.com>
2020-08-02 20:45:10 -0700

    sign in and sign up console logged/scripted now

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+console.log("script is running")
+
+// $(document).ready(function() {
+
+// $( "#button" ).click(function() {
+//   $( "#target" ).click();
+// });
+
+// $(document).on("click", "button.trails", deleteTodo);
+
+// function deleteTodo(event) {
+//     event.stopPropagation();
+//     var id = $(this).data("id");
+//     $.ajax({
+//       method: "GET",
+//       url: "/trails" 
+//     }).then(getTodos);
+//   }
+
+// getTodos();
+
+//   function getTodos() {
+//     $.get("/api/todos", function(data) {
+//       todos = data;
+//       initializeRows();
+//     });
+//   }
+// });
